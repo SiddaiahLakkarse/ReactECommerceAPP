ae58765bd18d8fc2183a245606105a9e808e60ff
Siddaiah Lakkarse <siddaiah.lakkarse@gmail.com>
2026-09-13 15:30:29 +0530

    Fix local app startup and storefront styling

--- src/Backend/ECommerce.Api/Program.cs
+++ src/Backend/ECommerce.Api/Program.cs
@@ -4,12 +4,16 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ECommerceDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ECommerceDbContext>(options =>
+{
+    if (builder.Environment.IsDevelopment())
+        options.UseInMemoryDatabase("ECommerceDevelopment");
+    else
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ECommerceDbContext>());
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ICartRepository, CartRepository>();
@@ -18,12 +22,24 @@ builder.Services.AddScoped<ProductCommands>();
 builder.Services.AddScoped<CartCommands>();
 builder.Services.AddScoped<OrderCommands>();
 builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ECommerceDbContext>();
+    db.Database.EnsureCreated();
+    if (!db.Products.Any())
+    {
+        db.Products.AddRange(
+            new ECommerce.Domain.Product("Stoneware Mug", "Hand-finished ceramic for your morning ritual.", 24.00m, 18),
+            new ECommerce.Domain.Product("Linen Throw", "Soft washed linen in a warm natural tone.", 68.00m, 12),
+            new ECommerce.Domain.Product("Field Notebook", "Hard-wearing recycled paper for good ideas.", 14.00m, 30));
+        db.SaveChanges();
+    }
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -31,9 +47,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseCors();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
